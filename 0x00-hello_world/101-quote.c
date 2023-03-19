dbc8e8f505963efd65b1b5e1858f44ca1ca1bab1
VeeDaisy <kaluwavee@gmail.com>
2023-03-19 08:35:59 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,8 @@
 
 /**
  * main - program prints line to standard error
- * Return 1
+ * Return: 1
  */
-
 int main(void)
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
